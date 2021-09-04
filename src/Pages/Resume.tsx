8ddd0c6bbe29d389b3dc344d3fbf7661ee92455c
milaandahiya <milaandahiya@gmail.com>
2021-09-04 01:34:27 -0500

    orange download

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import SystemUpdateIcon from '@material-ui/icons/SystemUpdate';
-import {Button} from "@material-ui/core";
+import {Button, createTheme, ThemeProvider} from "@material-ui/core";
 
 function Resume() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#BF5700'
+            }
+        },
+    });
+
     return (
         <object
             data="MilaanDahiyaResume.pdf"
@@ -11,15 +19,17 @@ function Resume() {
             width="100%"
         >
             <div className="container">
-                <Button
-                    size="large"
-                    variant="contained"
-                    color="primary"
-                    startIcon={<SystemUpdateIcon/>}
-                    href="MilaanDahiyaResume.pdf"
-                >
-                    Download
-                </Button>
+                <ThemeProvider theme={theme}>
+                    <Button
+                        size="large"
+                        variant="contained"
+                        color="primary"
+                        startIcon={<SystemUpdateIcon/>}
+                        href="MilaanDahiyaResume.pdf"
+                    >
+                        Download
+                    </Button>
+                </ThemeProvider>
                 <div className="shape-blob"/>
                 <div className="shape-blob two"/>
                 <div className="shape-blob three"/>
