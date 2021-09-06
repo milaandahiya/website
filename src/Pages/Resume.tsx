b2e477fb796c4b3375b1651650e101d70cf49459
milaandahiya <milaandahiya@gmail.com>
2021-09-06 00:07:23 -0500

    change to iframe

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -13,31 +13,34 @@ function Resume() {
     });
 
     return (
-        <object
-            data="MilaanDahiyaResume.pdf"
-            type="application/pdf"
-            height="1150px"
-            width="100%"
-        >
-            <div className="container">
-                <ThemeProvider theme={theme}>
-                    <Button
-                        size="large"
-                        variant="contained"
-                        color="primary"
-                        startIcon={<SystemUpdateIcon/>}
-                        href="MilaanDahiyaResume.pdf"
-                        className="download-button"
-                    >
-                        Download
-                    </Button>
-                </ThemeProvider>
-                <div className="shape-blob"/>
-                <div className="shape-blob two"/>
-                <div className="shape-blob three"/>
-                <div className="shape-blob four"/>
-            </div>
-        </object>
+        <div style={{backgroundColor: "#525659"}}>
+            <iframe
+                title="resume"
+                src="MilaanDahiyaResume.pdf"
+                height="1150px"
+                width="100%"
+                frameBorder="0"
+            >
+                <div className="container">
+                    <ThemeProvider theme={theme}>
+                        <Button
+                            size="large"
+                            variant="contained"
+                            color="primary"
+                            startIcon={<SystemUpdateIcon/>}
+                            href="MilaanDahiyaResume.pdf"
+                            className="download-button"
+                        >
+                            Download
+                        </Button>
+                    </ThemeProvider>
+                    <div className="shape-blob"/>
+                    <div className="shape-blob two"/>
+                    <div className="shape-blob three"/>
+                    <div className="shape-blob four"/>
+                </div>
+            </iframe>
+        </div>
     );
 }
 
